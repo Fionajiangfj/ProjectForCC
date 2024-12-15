487612f17fbce2d958d8a236fa3b049a561336a1
Fionajiangfj <67009589+Fionajiangfj@users.noreply.github.com>
2024-12-15 14:28:07 -0500

    modefy test1.py

--- Serverless-Inference-API/test1.py
+++ Serverless-Inference-API/test1.py
@@ -2,7 +2,7 @@ from huggingface_hub import InferenceClient
 
 client = InferenceClient(
     "cardiffnlp/twitter-roberta-base-sentiment-latest",
-    token="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",
+    token="huggingfacetoken",
 )
 
 result = client.text_classification("Today is a bad day")
